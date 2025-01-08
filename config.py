66a1a7a64780e1ea0c298eaa29d3dfb94cec8bd7
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-01-08 19:20:56 +0300

    test: test

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ PUBLIC_URL = os.environ.get('PUBLIC_URL')
 
 #DATABASE
 DB_USER = os.environ.get('DB_USER')
-DB_PASS = os.environ.get('DB_PASS')
+DB_PASS = os.environ.get('POSTGRES_PASSWORD')
 DB_HOST = os.environ.get('POSTGRES_HOST')
 DB_PORT = os.environ.get('DB_PORT')
 DB_NAME = os.environ.get('DB_NAME')
