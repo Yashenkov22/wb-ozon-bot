4fb2c2d30c496ca551fcfeff5fd0b47fc6d394f4
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-01-09 13:28:49 +0300

    test: test

--- handlers1/wb.py
+++ handlers1/wb.py
@@ -380,8 +380,8 @@ async def proccess_product_id(message: types.Message | types.CallbackQuery,
                 _basic_price = size.get('price').get('basic')
                 _product_price = size.get('price').get('product')
 
-                _basic_price = str(_basic_price)[:-2]
-                _product_price = str(_product_price)[:-2]
+                _basic_price = str(_basic_price)
+                _product_price = str(_product_price)
 
                 print('основная:', _basic_price)
                 print('актупльная:', _product_price)
