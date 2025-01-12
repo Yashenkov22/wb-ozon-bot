588a6386ad38b247411f570dcf852c0a9990dd88
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-01-12 16:05:21 +0300

    fix: fix

--- handlers1/base.py
+++ handlers1/base.py
@@ -149,7 +149,7 @@ async def redirect_to_(callback: types.CallbackQuery,
     # JobModel = Base.classes.apscheduler_jobs
 
     print(Base)
-    print(Base.metadata.classes)
+    print(Base.metadata.__dir__())
 ###
 
     # query = (
