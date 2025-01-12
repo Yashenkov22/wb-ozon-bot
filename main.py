878e8c4138e4fde359e305ac9423e6ccd743193f
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-01-12 14:39:29 +0300

    fix: fix event_loop

--- main.py
+++ main.py
@@ -81,7 +81,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-event_loop = asyncio.get_event_loop()
+# event_loop = asyncio.get_event_loop()
+event_loop = asyncio.new_event_loop()
+asyncio.set_event_loop(event_loop)
 config = Config(app=app,
                 loop=event_loop,
                 host='0.0.0.0',
