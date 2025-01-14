bf8fcdf52b1aafbda5f262120ad2c73799eb5d14
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-01-14 17:47:34 +0300

    test: test

--- handlers1/wb.py
+++ handlers1/wb.py
@@ -355,7 +355,7 @@ async def proccess_product_id(message: types.Message | types.CallbackQuery,
 
                 example_percent = 10
                 example_different = (_product_price * example_percent) / 100
-                example_price = _product_price - example_percent
+                example_price = _product_price - example_different
 
                 _text = f'Основная цена товара: {_basic_price}\nАктуальная цена товара: {_product_price}\nВведите <b>процент как число</b>.\nКогда цена товара снизится <b>на этот процент или ниже</b>, мы сообщим Вам.\nПример:\n Процент - {example_percent}\nОжидаемая(или ниже) цена товара: {_product_price} - {example_different} = {example_price}'
             else:
