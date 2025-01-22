0f92d8c81b662a40ff74656e718fef67efaa5574
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-01-22 15:48:49 +0300

    test: test wb in any message handler

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -128,10 +128,10 @@ async def push_check_wb_price(user_id: str,
                         await session.rollback()
                         print(ex)
                 # if _waiting_price == actual_price:
-                _text = f'WB товар\n{_name[:21]}\n<a href="{link}>"Ссылка на товар</a>\nЦена изменилась\nОбновленная цена товара: {_product_price} (было {actual_price})'
+                _text = f'WB товар\n{_name[:21]}\n<a href="{link}">Ссылка на товар</a>\nЦена изменилась\nОбновленная цена товара: {_product_price} (было {actual_price})'
 
                 if _waiting_price and _waiting_price >= _product_price:
-                    _text = f'WB товар\nНазвание: {name[:21]}\n<a href="{link}>"Ссылка на товар</a>\nЦена товара, которую(или ниже) Вы ждали ({_waiting_price})\nОбновленная цена товара: {_product_price} (было {actual_price})'
+                    _text = f'WB товар\nНазвание: {name[:21]}\n<a href="{link}">Ссылка на товар</a>\nЦена товара, которую(или ниже) Вы ждали ({_waiting_price})\nОбновленная цена товара: {_product_price} (было {actual_price})'
                     
                     _kb = create_remove_kb(user_id,
                                             product_id,
