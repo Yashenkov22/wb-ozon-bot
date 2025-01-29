c96207d28a3106d470cc636ed377c8d2cb713989
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-01-29 21:27:18 +0300

    fix: edit ozon/wb add handler finish message

--- handlers/ozon.py
+++ handlers/ozon.py
@@ -300,7 +300,7 @@ async def proccess_product(message: types.Message | types.CallbackQuery,
 
         # _text = f'Ваш товар: {link}\nНачальная цена: {start_price}\nАктуальная цена: {product_price}\nУстановленная скидка: {sale}\nОжидаемая цена: {waiting_price}'
 
-        _text = f'Название: <a href="{link}">{_product_name}</a>\nМаркетплейс: Ozon\n\nОсновная цена(без Ozon карты): {_d.get('price', 0)}\nНачальная цена: {start_price}\nАктуальная цена: {start_price}\n\nОтслеживается изменение цены на: {sale}\nОжидаемая цена: {start_price - sale}'
+        _text = f'Название: <a href="{link}">{_product_name}</a>\nМаркетплейс: Ozon\n\nОсновная цена(без Ozon карты): {_d.get("price", 0)}\nНачальная цена: {start_price}\nАктуальная цена: {start_price}\n\nОтслеживается изменение цены на: {sale}\nОжидаемая цена: {start_price - sale}'
 
 
         if msg:
