e83c9ebd079b2529eaf61e66a774954fda16efcf
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-01-30 13:39:52 +0300

    test: test ozom link samolet

--- handlers/ozon.py
+++ handlers/ozon.py
@@ -198,7 +198,7 @@ async def proccess_product(message: types.Message | types.CallbackQuery,
 
                 res = await response.text()
 
-        # print('ОТВЕТ ОТ АПИ',res)
+        print('ОТВЕТ ОТ АПИ',res)
 
         new_short_link = res.split('|')[0]
 
