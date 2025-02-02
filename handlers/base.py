dfef45c42c02f7f029d784776fdfa4c651bddd58
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-02-02 17:20:02 +0300

    test: test all_product_list

--- handlers/base.py
+++ handlers/base.py
@@ -314,7 +314,8 @@ async def get_all_products_by_user(message: types.Message | types.CallbackQuery,
         'product_list': product_list,
     }
 
-    await show_product_list(view_product_dict)
+    await show_product_list(view_product_dict,
+                            message.from_user.id)
     # msg: tuple = data.get('msg')
     # _text = 'Отправьте ссылку на товар'
 
