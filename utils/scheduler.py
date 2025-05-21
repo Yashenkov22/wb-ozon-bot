adfa8e7dff73f65b926331e0180d90c710d004df
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-05-21 17:59:31 +0300

    fix: turn on update scheduler jobs

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -2587,8 +2587,8 @@ async def startup_update_scheduler_jobs(scheduler: AsyncIOScheduler):
                     modify_func = new_push_check_ozon_price
                 # else:
                 #     modify_func = push_check_ozon_price
-            job.modify(func=modify_func,
-                       trigger=scheduler_cron)   
+                job.modify(func=modify_func,
+                        trigger=scheduler_cron)   
             
         elif job.id.find('delete_msg_task') != -1:
             user_id = job.id.split('_')[-1]
