cd5acab1c35f772328a7434e6c51c0f4fb3734f0
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-05-22 14:08:00 +0300

    test: test background tasks

--- background/tasks.py
+++ background/tasks.py
@@ -400,6 +400,10 @@ async def new_push_check_wb_price(cxt,
             if check_price:
                 _text = 'цена не изменилась'
                 print(f'{_text} user {user_id} product {name}')
+                
+                if user_id == int(DEV_ID):
+                    await bot.send_message(chat_id=user_id,
+                                           text=f'{_text} user {user_id} product {name}')
                 return
             
             else:
