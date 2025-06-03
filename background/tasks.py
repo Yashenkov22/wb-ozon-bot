63bf844f340b71016d9b4e4a9982e162028b6bfc
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-06-03 22:12:37 +0300

    fix: edit task

--- background/tasks.py
+++ background/tasks.py
@@ -411,10 +411,10 @@ async def new_push_check_wb_price(cxt,
                     _text = 'цена не изменилась'
                     print(f'{_text} user {user_id} product {name}')
                     
-                    if int(user_id) == int(DEV_ID):
-                        await bot.send_message(chat_id=user_id,
-                                            text=f'{_text} user {user_id} product {name}')
-                    return
+                    # if int(user_id) == int(DEV_ID):
+                    #     await bot.send_message(chat_id=user_id,
+                    #                         text=f'{_text} user {user_id} product {name}')
+                    # return
                 
                 else:
                     update_query = (
